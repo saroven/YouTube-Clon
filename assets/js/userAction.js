0b9344768c2d651f61b8012f4dbba3fffbb5b3eb
= <=>
2021-08-31 00:30:01 +0600

    create subscribe function

--- assets/js/userAction.js
+++ assets/js/userAction.js
@@ -1,3 +1,9 @@
 function subscribe(userTo,userFrom, button){
-
+    if (userTo == userFrom){
+        alert("You can't subscribe yourself");
+        return;
+    }
+    $.post("ajax/subscribe.php").done(function (){
+        console.log("done");
+    })
 }
