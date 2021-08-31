5dbd8fa3fb37ab0e6244cdb8258ca71f49550a9b
= <=>
2021-09-01 01:22:17 +0600

    insert comment into database

--- ajax/postComment.php
+++ ajax/postComment.php
@@ -2,7 +2,20 @@
     require '../include/config.php';
 
     if (isset($_POST['commentText']) && isset($_POST['postedBy']) && isset($_POST['videoId'])){
-        echo "success";
+        $query = $conn->prepare("INSERT INTO comments(postedBy, videoId, responseTo, body)
+                                        VALUES (:postedBy, :videoId, :responseTo, :body)");
+        $postedBy = $_POST['postedBy'];
+        $videoId = $_POST['videoId'];
+        $responseTo = $_POST['responseTo'];
+        $commentText = $_POST['commentText'];
+
+        $query->bindParam(":postedBy", $postedBy);
+        $query->bindParam(":videoId", $videoId);
+        $query->bindParam(":responseTo", $responseTo);
+        $query->bindParam(":body", $commentText);
+
+        $query->execute();
+        //return new comment
     }else{
         echo "One or more parameter are not passed into the subscribe.php file!";
     }
