ab2076ddf3078d5a01717c59d759dea929384656
= <=>
2021-10-06 00:08:19 +0600

    inserting dislike and getting response from ajax

--- assets/js/commentAction.js
+++ assets/js/commentAction.js
@@ -39,7 +39,23 @@ function likeComment(commentId, button, videoId) {
     });
 }
 function dislikeComment(commentId, button, videoId) {
+     $.post('ajax/dislikeComment.php', {commentId: commentId, videoId: videoId }).done(function (numToChange) {
+        let dislikeButton = $(button);
+        let likeButton = $(button).siblings(".likeButton");
+        dislikeButton.addClass("active");
+        likeButton.removeClass("active");
 
+        let likesCount = $(button).siblings(".likesCount");
+        updateLikeValue(likesCount, numToChange);
+
+        if (numToChange > 0){
+            dislikeButton.removeClass("active");
+            dislikeButton.find("img:first").attr("src","assets/images/icons/thumb-down.png");
+        }else{
+            dislikeButton.find("img:first").attr("src","assets/images/icons/thumb-down-active.png");
+        }
+        likeButton.find("img:first").attr("src","assets/images/icons/thumb-up.png");
+    });
 }
 function updateLikeValue(element, number) {
     let likeCount = element.text() || 0;
